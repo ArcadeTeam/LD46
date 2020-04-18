bc677feacbd3f39e5a1ae96f19516f1a21cb74be
Nestorferrando <ferrando.nestor@gmail.com>
2020-04-18 18:48:28 +0200

    el pato esprinteao

--- LD46/Assets/Scripts/DuckPlayer/DuckController.cs
+++ LD46/Assets/Scripts/DuckPlayer/DuckController.cs
@@ -8,8 +8,10 @@ public class DuckController : Duck
     public float Speed = 1f;
     public float JumpHeight = 4f;
 
-    public float DashForce = 5f;
-    public float timeBetweenDash = 3f;
+    public float SprintSpeed = 2f;
+    public float SprintTime = 1.5f;
+    public float timeBetweenSprint = 3f;
+    private float lastSprintTime = -999f;
 
     public LayerMask Ground;
     
@@ -21,7 +23,6 @@ public class DuckController : Duck
     private CameraFollower cameraFollower;
 
     public List<BabyDuckController> nearBabies;
-    private float lastDash = -999f;
 
     private Vector3 ori1;
     private Vector3 ori2;
@@ -113,11 +114,10 @@ public class DuckController : Duck
             }
 
             //dash logic
-            if (Input.GetButtonDown("Fire1")) {
-                if (Time.fixedTime - lastDash >= timeBetweenDash)
+            if (Input.GetButtonDown("Fire1") && _isGrounded) {
+                if (Time.fixedTime - lastSprintTime >= timeBetweenSprint)
                 {
-                    lastDash = Time.fixedTime;
-                    _body.AddForce(transform.forward.normalized * DashForce, ForceMode.Impulse);
+                    lastSprintTime = Time.fixedTime;
                 }
             }
 
@@ -128,6 +128,11 @@ public class DuckController : Duck
                 }
             }
         }
+    }
+
+    private bool isSprinting()
+    {
+        return (Time.fixedTime - lastSprintTime < SprintTime);
     }
 
     private void checkOnFloor() 
@@ -149,9 +154,12 @@ public class DuckController : Duck
     {
         if (!dead)
         {
+            var speed = Speed;
+            if (isSprinting()) speed = SprintSpeed;
+
             var orientation = orientatedInput;
             if (!_isGrounded) orientation = Vector3.Lerp(orientation, lastOrientationWhenGrounded, 0.25f);
-            _body.MovePosition(_body.position + orientation * Speed * Time.fixedDeltaTime);
+            _body.MovePosition(_body.position + orientation * speed * Time.fixedDeltaTime);
         }
         else
         {
