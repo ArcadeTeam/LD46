a68eff6cb938307f1ff267530cc6c5cc77ff1f0f
Alvaro Lopez <aedon20@gmail.com>
2020-04-18 14:45:50 +0200

    traffic and people

---
+++ LD46/Assets/Scripts/Obstacles/CarController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CarController : MonoBehaviour
+{
+    private float distanceThreshold = 5f;
+    public Transform waypoints;
+    private List<Transform> positions;
+
+    public Transform currentTarget;
+    private int currentPosition;
+
+    private NavMeshAgent agent;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        positions = new List<Transform>();
+
+        foreach (Transform child in waypoints)
+        {
+            //Debug.Log(child.name);
+            positions.Add(child);
+        }
+
+        InitializeFirstPosition();
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = currentTarget.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPatrolNextPosition();
+    }
+
+    private void InitializeFirstPosition()
+    {
+        if (currentTarget != null)
+            for (int i = 0; i < waypoints.childCount; i++) 
+            { 
+                //Debug.Log(child.name);
+                if (currentTarget == waypoints.GetChild(i))
+                    currentPosition = i;
+            }
+        
+        else
+            currentPosition = 0;
+        currentTarget = positions[currentPosition];
+    }
+
+
+    private void CheckPatrolNextPosition()
+    {
+        //Debug.Log((Vector3.Distance(transform.position, currentTarget.position) < distanceThreshold));
+        if (Vector3.Distance(transform.position, currentTarget.position) < distanceThreshold)
+        {
+            UpdateCurrentPosition();
+        }
+    }
+
+    private void UpdateCurrentPosition()
+    {
+        currentPosition = GetNextPosition();
+        currentTarget = positions[currentPosition];
+        /*Debug.Log(currentPosition);
+        Debug.Log(currentTarget);*/
+        agent.destination = currentTarget.position;
+    }
+
+    private int GetNextPosition()
+    {
+        int nextPosition = currentPosition + 1;
+
+        if (nextPosition >= positions.Count)
+            nextPosition = 0;
+
+        return nextPosition;
+    }
+
+}
---
+++ LD46/Assets/Scripts/Obstacles/HumanController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HumanController : MonoBehaviour
+{
+
+    private float walkRadius;
+    private float changePathTimeout = 5f;
+    private float walkThreshold = 2f;
+
+    private Vector3 lastPosition;
+
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = GetRandomPoint();
+
+        walkRadius = Random.Range(10f, 50f);
+        lastPosition = transform.position;
+        InvokeRepeating("ChangePath", changePathTimeout, Random.Range(3f, changePathTimeout));
+    }
+
+    private void ChangePath()
+    {
+        if (Vector3.Distance(transform.position, lastPosition) < walkThreshold)
+        {
+            agent.destination = GetRandomPoint();
+        }
+
+        lastPosition = transform.position;
+    }
+
+
+    private Vector3 GetRandomPoint()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+        randomDirection += transform.position;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, walkRadius, 1);
+
+        return hit.position;
+    }
+}
