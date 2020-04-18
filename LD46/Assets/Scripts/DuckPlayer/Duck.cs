38230a61847f3898c533c6dd5c5d34fd47b97e66
Alvaro Lopez <aedon20@gmail.com>
2020-04-18 18:42:14 +0200

    duck

---
+++ LD46/Assets/Scripts/DuckPlayer/Duck.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Duck : MonoBehaviour
+{
+    protected Rigidbody _body;
+    protected bool dead = false;
+    void Awake()
+    {
+        _body = GetComponent<Rigidbody>();
+    }
+
+    public void killDuck(Vector3 impactOrientation, float impactSpeed = 1.0f)
+    {
+        dead = true;
+        GetComponent<Rigidbody>().freezeRotation = false;
+        _body.velocity = impactOrientation.normalized * impactSpeed;
+        _body.AddTorque(new Vector3(0f, 10f, 10f));
+    }
+}
