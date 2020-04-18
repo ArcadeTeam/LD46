11f076fbfc4781a17db0c2ae1e7c262c142cef55
Alvaro Lopez <aedon20@gmail.com>
2020-04-18 19:30:05 +0200

    falling object zone

---
+++ LD46/Assets/Scripts/Obstacles/FallingObject/FallingObjectsManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjectsManager : MonoBehaviour
+{
+
+    public GameObject[] items;
+    private float yOffset = 20f;
+
+    private bool objectSpawned = false;
+    private float spawnCooldownTime = 10f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("BabyDuck")) {
+            InstanceFallingObject(other.transform.position);
+        }
+    }
+
+    private void InstanceFallingObject(Vector3 position)
+    {
+        if (items.Length == 0 || objectSpawned) return;
+
+        objectSpawned = true;
+        var item = items[Random.Range(0, items.Length)];
+        var pos = new Vector3(position.x, position.y + yOffset, position.z);
+        Instantiate(item, pos, item.transform.rotation);
+        Invoke("SpawnCooldown", spawnCooldownTime);
+    }
+
+    private void SpawnCooldown()
+    {
+        objectSpawned = false;
+    }
+
+}
