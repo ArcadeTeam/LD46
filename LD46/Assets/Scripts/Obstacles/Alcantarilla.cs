d3117668f2d3a7c57f81324ba01f892da06c1bb6
Adrian Guevara <adrianguevaralonso@gmail.com>
2020-04-18 18:18:37 +0200

    Alcantarilla implemented

---
+++ LD46/Assets/Scripts/Obstacles/Alcantarilla.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alcantarilla : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision col) {
+        if (col.gameObject.CompareTag("BabyDuck") || col.gameObject.CompareTag("Player")) {
+            col.gameObject.GetComponent<Collider>().enabled = false;
+            if (col.gameObject.CompareTag("Player")) {
+                Camera.main.GetComponent<CameraFollower>().enabled = false;
+            }
+        }
+    }
+}
