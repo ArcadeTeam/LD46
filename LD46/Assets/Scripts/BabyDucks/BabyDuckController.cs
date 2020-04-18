8ba746ee2e719bfe7c48d300f9604fae5988ded1
Adrian Guevara <adrianguevaralonso@gmail.com>
2020-04-18 14:14:21 +0200

    Basic baby ducks IA

---
+++ LD46/Assets/Scripts/BabyDucks/BabyDuckController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BabyDuckController : MonoBehaviour
+{
+    public Transform destination;
+    NavMeshAgent agent;
+    public int distractionProbability = 0;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update() {
+        agent.destination = destination.position;
+    }
+
+    public void Distract(Transform newDestination) {
+        distractionProbability += 10;
+        if(distractionProbability > Random.Range(0, 100)) {
+            destination = newDestination;
+        }
+    }
+
+    public void GoWithMom(Transform newDestination) {
+        distractionProbability = 0;
+        destination = newDestination;
+    }
+}
