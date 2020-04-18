0a0f24eb79503bd2acfb1b58903ef8e4c330a65b
Nestorferrando <ferrando.nestor@gmail.com>
2020-04-18 13:05:58 +0200

    added duckplayer y duckcamera

---
+++ LD46/Assets/Scripts/Camera/CameraFollower.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+
+    private GameObject duckObject;
+    public float cameraDistance = 6f;
+    public float cameraSpeed = 0.015f;
+    public Vector3 cameraPositionRelative = new Vector3(-1f, -1f, 1f);
+
+    private List<Vector3> filteredPositions = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        duckObject = FindObjectOfType<DuckController>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.forward = cameraPositionRelative;
+        var distance = cameraPositionRelative.magnitude * cameraDistance;
+        Vector3 desiredCameraLocation = duckObject.transform.position - cameraPositionRelative* distance;
+        Vector3 difference = (transform.position - desiredCameraLocation);
+        filteredPositions.Add(difference);
+        if (filteredPositions.Count>4) filteredPositions.RemoveAt(0);
+        Vector3 average = Vector3.zero;
+
+        transform.position -= difference * cameraSpeed;
+
+        
+    }
+}
---
+++ LD46/Assets/Scripts/DuckPlayer/DuckController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuckController : MonoBehaviour
+{
+    public float Speed = 1f;
+    public float JumpHeight = 2f;
+    public LayerMask Ground;
+
+    private Rigidbody _body;
+    private Vector3 _inputs = Vector3.zero;
+    private bool _isGrounded = true;
+
+    void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+       ;
+        
+    }
+
+    private Vector3 orientatedInput;
+
+    void Update() {
+
+        _isGrounded = isOnFloor();
+
+
+        _inputs = Vector3.zero;
+        _inputs.x = Input.GetAxis("Horizontal");
+        _inputs.z = Input.GetAxis("Vertical");
+        if (_inputs != Vector3.zero)
+        {
+
+            var cameraPos = Camera.main.GetComponent<CameraFollower>().cameraPositionRelative;
+            var camPos2 = new Vector3(cameraPos.x,0,cameraPos.z);
+            transform.forward = camPos2;
+
+            orientatedInput = transform.localRotation * _inputs;
+            transform.forward = orientatedInput;
+
+
+
+          //  transform.forward = _inputs;
+        } else orientatedInput = Vector3.zero;
+
+        if (Input.GetButtonDown("Jump")/* && _isGrounded*/)
+        {
+            _body.AddForce(Vector3.up * Mathf.Sqrt(JumpHeight * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+    }
+
+    private bool isOnFloor() 
+    {
+        float rayDistance = 100;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit, rayDistance))
+        {
+            var distanceToFloor = GetComponent<CapsuleCollider>().height * 0.5f;
+            if (hit.distance <= distanceToFloor*1.001f) return true;
+        }
+
+        return false;
+    }
+
+
+    void FixedUpdate()
+    {
+        _body.MovePosition(_body.position + orientatedInput * Speed * Time.fixedDeltaTime);
+    }
+}
