b984ae67db3b35e7c70639b9851b70fbf404054c
Adrian Guevara <adrianguevaralonso@gmail.com>
2020-04-19 16:26:55 +0200

    Fixed Honk

--- LD46/Assets/Scripts/UI/Honk.cs
+++ LD46/Assets/Scripts/UI/Honk.cs
@@ -11,7 +11,7 @@ public class Honk : SpeakBubbles
     }
 
     // Update is called once per frame
-    void Update() {
+    void FixedUpdate() {
         firstImg.GetComponent<RectTransform>().localRotation = Quaternion.Euler(new Vector3(0, 0, -player.transform.rotation.eulerAngles.y - 90f));
         secondImg.GetComponent<RectTransform>().localRotation = Quaternion.Euler(new Vector3(0, 0, -player.transform.rotation.eulerAngles.y - 90f));
     }
