de9cdf5c4d4769cb30106eb7934fb1ea9cb464c7
Adrian Guevara <adrianguevaralonso@gmail.com>
2020-04-19 22:19:15 +0200

    Cosas

--- LD46/Assets/Scripts/DuckPlayer/Duck.cs
+++ LD46/Assets/Scripts/DuckPlayer/Duck.cs
@@ -6,14 +6,21 @@ public class Duck : MonoBehaviour
 {
     protected Rigidbody _body;
     protected bool dead = false;
+
+    public ParticleSystem splash;
+    private bool canSplah = true;
+    private float waterSplashCooldown = 1f;
+
     void Awake()
     {
+        splash.Stop();
         _body = GetComponent<Rigidbody>();
     }
 
     public void killDuck(Vector3 impactOrientation, float impactSpeed = 1.0f)
     {
         if (gameObject.CompareTag("Player") && !dead) {
+            gameObject.transform.FindChild("")
             GameObject.Find("GameManager").GetComponent<GameManager>().GameOver();
         }
         dead = true;
@@ -22,17 +29,19 @@ public class Duck : MonoBehaviour
         _body.AddTorque(new Vector3(0f, 10f, 10f));
     }
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    if (dead) return;
+    public void WaterSplash()
+    {
+        splash.Play();
+        /*if (canSplah)
+        {
+            canSplah = false;
+            splash.Play();
+            Invoke("WaterSplashCooldown", waterSplashCooldown);
+        }*/
+    }
 
-    //    Debug.Log(collision.relativeVelocity);
-    //    if (collision.relativeVelocity.y > 15)
-    //    {
-    //        var direction = -collision.impulse.normalized;
-    //        var impact = new Vector3(direction.x, 0.2f, direction.z);
-
-    //        killDuck(impact, 30f);
-    //    }
-    //}
+    private void WaterSplashCooldown()
+    {
+        canSplah = true;
+    }
 }
