38ad9b86f728adec2f3dcd184f99ce56be1571d1
Alvaro Lopez <aedon20@gmail.com>
2020-04-19 08:30:44 +0200

    falling objects can kill

--- LD46/Assets/Scripts/Obstacles/FallingObject/FallingObject.cs
+++ LD46/Assets/Scripts/Obstacles/FallingObject/FallingObject.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FallingObject : MonoBehaviour
 {
-
+    public bool canKill = true;
     private Projector projector;
     private bool hasBeenHit = false;
 
@@ -19,6 +19,14 @@ public class FallingObject : MonoBehaviour
         {
             hasBeenHit = true;
             projector.enabled = false;
+
+            if (canKill && (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("BabyDuck")))
+            {
+                var direction = -collision.impulse.normalized;
+                var impact = new Vector3(direction.x, 0.2f, direction.z);
+
+                collision.gameObject.GetComponent<Duck>().killDuck(impact, 30f);
+            }
         }
     }
 
