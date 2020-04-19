f32fa099ecf9fbf6ee3614dfc5a62b1d0a819d35
Alvaro Lopez <aedon20@gmail.com>
2020-04-20 00:01:37 +0200

    sfx

---
+++ LD46/Assets/Scripts/Water/FloatingObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingObject : MonoBehaviour
+{
+    public float yOffset = 0.2f;
+    public float velocity = 0.08f;
+
+    private Vector3 initialPosition;
+    private Vector3 upPosition;
+    private Vector3 downPosition;
+    private bool goUp = true;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+        upPosition = new Vector3(initialPosition.x, initialPosition.y + yOffset, initialPosition.z);
+        downPosition = new Vector3(initialPosition.x, initialPosition.y - yOffset, initialPosition.z);
+    }
+
+    private void Update()
+    {
+        CheckNextPosition();
+        Move();
+    }
+
+    private void CheckNextPosition()
+    {
+        if (goUp)
+        {
+            if (transform.position.y > upPosition.y)
+                goUp = false;
+        }
+        else
+        {
+            if (transform.position.y < downPosition.y)
+                goUp = true;
+        }
+    }
+
+    private void Move()
+    {
+        if (goUp) transform.Translate(new Vector3(0, Time.deltaTime * velocity, 0));
+        else transform.Translate(new Vector3(0, Time.deltaTime * -velocity, 0));
+    }
+
+
+}
