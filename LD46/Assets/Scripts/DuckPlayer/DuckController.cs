c104d5c21bf810c355b7e4c2ec1300646d110a47
Nestorferrando <ferrando.nestor@gmail.com>
2020-04-19 12:02:17 +0200

    some fixes

--- LD46/Assets/Scripts/DuckPlayer/DuckController.cs
+++ LD46/Assets/Scripts/DuckPlayer/DuckController.cs
@@ -66,10 +66,11 @@ public class DuckController : Duck
             //applying gravity
             if (planning)
             {
-                if (_body.velocity.y > -0.1f)
+                _body.AddForce(0.2f * Physics.gravity * (_body.mass * _body.mass));
+                Debug.Log(_body.velocity.y);
+                if (_body.velocity.y < -0.7f)
                 {
-                    _body.AddForce(0.2f * Physics.gravity * (_body.mass * _body.mass));
-                    _body.velocity = new Vector3(_body.velocity.x, _body.velocity.y, _body.velocity.z);
+                    _body.velocity = new Vector3(_body.velocity.x, -0.7f, _body.velocity.z);
                 }
             }
             else
