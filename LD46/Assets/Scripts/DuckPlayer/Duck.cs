1088c4a7abb189201c16bf820a5d177705c49d25
Alvaro Lopez <aedon20@gmail.com>
2020-04-19 21:04:52 +0200

    duck water splash

--- LD46/Assets/Scripts/DuckPlayer/Duck.cs
+++ LD46/Assets/Scripts/DuckPlayer/Duck.cs
@@ -6,8 +6,14 @@ public class Duck : MonoBehaviour
 {
     protected Rigidbody _body;
     protected bool dead = false;
+
+    public ParticleSystem splash;
+    private bool canSplah = true;
+    private float waterSplashCooldown = 1f;
+
     void Awake()
     {
+        splash.Stop();
         _body = GetComponent<Rigidbody>();
     }
 
@@ -22,17 +28,19 @@ public class Duck : MonoBehaviour
         _body.AddTorque(new Vector3(0f, 10f, 10f));
     }
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    if (dead) return;
+    public void WaterSplash()
+    {
+        splash.Play();
+        /*if (canSplah)
+        {
+            canSplah = false;
+            splash.Play();
+            Invoke("WaterSplashCooldown", waterSplashCooldown);
+        }*/
+    }
 
-    //    Debug.Log(collision.relativeVelocity);
-    //    if (collision.relativeVelocity.y > 15)
-    //    {
-    //        var direction = -collision.impulse.normalized;
-    //        var impact = new Vector3(direction.x, 0.2f, direction.z);
-
-    //        killDuck(impact, 30f);
-    //    }
-    //}
+    private void WaterSplashCooldown()
+    {
+        canSplah = true;
+    }
 }
---
+++ LD46/Assets/Scripts/Water/WaterCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCollider : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        CheckWaterSplash(collision);
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        CheckWaterSplash(collision);
+    }
+
+    private void CheckWaterSplash(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("BabyDuck"))
+        {
+            collision.gameObject.GetComponent<Duck>().WaterSplash();
+        }
+    }
+
+}
