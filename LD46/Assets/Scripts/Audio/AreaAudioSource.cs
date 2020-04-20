768a419333562f906a1bcadd7961b885b8a5ce30
Alvaro Lopez <aedon20@gmail.com>
2020-04-20 10:43:06 +0200

    area audio source

---
+++ LD46/Assets/Scripts/Audio/AreaAudioSource.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaAudioSource : MonoBehaviour
+{
+
+    private AudioSource audio;
+    private bool entering = false;
+
+    private void Awake()
+    {
+        audio = GetComponent<AudioSource>();
+        audio.volume = 0;
+    }
+
+    private void Update()
+    {
+        if (entering)
+        {
+            audio.volume += Time.deltaTime;
+        } else
+        {
+            audio.volume -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            entering = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            entering = false;
+        }
+    }
+
+
+}
